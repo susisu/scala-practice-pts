1073fd65b31b67c987366899d09b97c31f96b140
Susisu <susisu2413@yahoo.co.jp>
2017-12-12 12:37:54 +0900

    Add PTS object

--- src/main/scala/pts/PTS.scala
+++ src/main/scala/pts/PTS.scala
@@ -1,7 +1,8 @@
 package pts
 
+case class PTS(axioms: PTS.Axioms, rules: PTS.Rules)
+
 object PTS {
-  def main(args: Array[String]) {
-    println("Hello, world!")
-  }
+  type Axioms = Map[String, String]
+  type Rules = Map[(String, String), String]
 }
